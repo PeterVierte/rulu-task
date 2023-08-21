e47353c476367de9a959f74a545275443187a1e9
tochkasopryazheniya <tochkasopryazheniya@gmail.com>
2023-08-21 17:53:31 +0500

    project is done

---
+++ main.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import AdmZip from 'adm-zip';
+import iconv  from 'iconv-lite';
+import * as fs from "fs";
+import xml2js from 'xml2js';
+
+const getInfo = async () => {
+    try {
+        const response = await fetch('http://www.cbr.ru/s/newbik');
+        const buffer = await response.buffer();
+        fs.writeFileSync('file.zip', buffer);
+
+        const zip = new AdmZip('file.zip', {});
+        const zipEntry = zip.getEntries()[0];
+        const xmlBuffer = zipEntry.getData();
+        const xmlString = iconv.decode(xmlBuffer, 'win1251');
+
+        const parser = new xml2js.Parser();
+        const parsedData = await parser.parseStringPromise(xmlString);
+        const directory = parsedData['ED807']['BICDirectoryEntry'];
+
+        const dataForDB = [];
+        directory.forEach(item => {
+            if(item.hasOwnProperty('Accounts')) {
+                item['Accounts'].forEach(subItem => {
+                    const info = {
+                        bic: item['$']['BIC'],
+                        name: item['ParticipantInfo'][0]['$']['NameP'],
+                        account: subItem['$']['Account']
+                    }
+                    dataForDB.push(info);
+                })
+            }
+        })
+
+        return dataForDB;
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+getInfo();
